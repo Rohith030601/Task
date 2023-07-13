f93d48689239bb23b8adfee7cd06a42c2ba71710
Rohith030601 <rohithabburi2001@gmail.com>
2023-07-13 10:40:57 +0530

    Task Completed

---
+++ Task_Task/src/main/java/com/task/TaskApplication.java
@@ -0,0 +1,22 @@
+package com.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
+/*URL's of BackEnd Services
+ * 
+ *Insert Data= 		 	http://localhost:2000
+ *Retrieving Data:   	http://localhost:2000/getAll
+ *Finding Average:   	http://localhost:2000/average
+ *Finding Middle No: 	http://localhost:2000/middle
+ *Finding Duplicates:	http://localhost:2000/duplicates
+ *Finding even-odd-averages: http://localhost:2000/even-odd-averages
+ */
